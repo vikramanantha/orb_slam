6988738b9babaede091aa2f078bb212420c5c212
Vikram Anantha <markiv@umich.edu>
2025-02-25 15:55:42 -0500

    bob the builder

---
+++ catkin_ws/build/orb_slam/CMakeFiles/Mono_Inertial.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/ORB_SLAM3/catkin_ws/src/orb_slam/src/ros_mono_inertial.cc" "/ORB_SLAM3/catkin_ws/build/orb_slam/CMakeFiles/Mono_Inertial.dir/src/ros_mono_inertial.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "ORB_SLAM3_INCLUDE_DIR=\"/ORB_SLAM3/catkin_ws/src/orb_slam/../../../include\""
+  "ORB_SLAM3_ROOT_DIR=\"/ORB_SLAM3/catkin_ws/src/orb_slam/../../..\""
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"orb_slam\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/ORB_SLAM3/catkin_ws/src/orb_slam/../../../include/CameraModels"
+  "/ORB_SLAM3/catkin_ws/src/orb_slam/../../../include"
+  "/ORB_SLAM3/catkin_ws/src/orb_slam/../../.."
+  "/usr/include/opencv"
+  "/opt/ros/melodic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/opt/ros/melodic/include"
+  "/ORB_SLAM3/catkin_ws/src/orb_slam/include"
+  "/usr/include/eigen3"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
